c820d04bd55c084b4180082ea7612daf4ed515d0
Danny <danny22marin@gmail.com>
2023-12-10 13:45:26 -0500

    Hooks1_Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,60 @@
-import './App.css'
-import Header from './components/Header';
-import { TaskList } from './components/TaskList';
-
-const exampleTaskList = [
-  { name:"Task1"},
-  { name:"Task2"},
-  { name:"Task3"},
-]
+import "./App.css";
+import Header from "./components/Header";
+import { TaskList } from "./components/TaskList";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [taskItems, setTaskItems] = useState([
+    // { title: "Example", description: "Example", complete: false },
+  ]);
+
+  function createNewTask(taskTitle) {
+    if (!taskItems.find((task) => task.title === taskTitle)) {
+      setTaskItems([
+        ...taskItems,
+        { title: taskTitle, description: taskTitle, complete: false },
+      ]);
+    }
+  }
+
+  const toggleTask = (task) => {
+    setTaskItems(taskItems.map((t) => (t.title == task.title ? {...t, complete: !t.complete} : t )));
+  }
+
+  useEffect(() => {
+    let data = localStorage.getItem("task");
+    if (data) {
+      setTaskItems(JSON.parse(data));
+    }
+  }, []);
+
+  const cleanTasks = () => { 
+    setTaskItems(taskItems.filter(task => !task.complete))
+  }
+
+  const deleteTask = (taskTitle) => {
+    setTaskItems(taskItems.filter(task => task.title !== taskTitle))
+    
+  };
+
+  const editTask = (oldTitle, newTitle) => {
+    setTaskItems(taskItems.map((task) => task.title === oldTitle ? {...task, title: newTitle} : task))
+  }
+
+  useEffect(() => {
+    localStorage.setItem("task", JSON.stringify(taskItems));
+  }, [taskItems]);
+
   return (
     <>
-    <Header />  
-    <TaskList  list={exampleTaskList}/>
+      <Header />
+      <TaskList 
+      createNewTask={createNewTask} 
+      list={taskItems} 
+      toggleTask={toggleTask} 
+      cleanTasks={cleanTasks} 
+      deleteTask={deleteTask}
+      editTask={editTask}/>
     </>
   );
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,24 +1,71 @@
+import { useState } from "react";
 import { Task } from "./Task";
 
 export const TaskList = (props) => {
+  const [newTask, setNewTask] = useState("");
 
-    function handleClick() {
-            console.log("Add task")
-        }
+  function handleChange(e) {
+    setNewTask(e.target.value);
+  }
 
-    const { list } = props;
-
-        return (
-        <>
-        <input placeholder="New Task"></input>
-        <button onClick={handleClick}>+</button>
-        <ul>
-            {
-                list.map((task) => (<Task name={task.name}/>))
-            }            
-        </ul>
-        <button>Clear All</button>
-        
-        </>
+  function handleClick() {
+    props.createNewTask(newTask);
+    setNewTask("");
+  }
+  
+  const { list, toggleTask, cleanTasks, deleteTask, editTask } = props;
+  
+  const taskTableRows = (completeValue) => {
+    return (      
+      list
+        .filter(task => task.complete === completeValue)
+        .map((task) => (
+        <Task key={task.title}
+        title={task.title} 
+        complete = {task.complete}
+        toggleTask={toggleTask}
+        deleteTask={deleteTask}
+        editTask={editTask}/>
+      ))
     );
-}
+  }
+
+  function handleClear () {
+    if (window.confirm('Are you sure about deleting tasks?')) {
+      cleanTasks();
+    }
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="New Task"
+        value={newTask}
+        onChange={handleChange}
+      ></input>
+      <button onClick={handleClick}>+</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Your pending tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskTableRows(false)}
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Your completed tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskTableRows(true)}
+        </tbody>
+      </table>
+      <button onClick={handleClear}>Clear completed tasks</button>
+    </>
+  );
+};
